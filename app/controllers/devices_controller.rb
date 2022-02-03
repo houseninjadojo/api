6ae4beab07d33c267e1271df287812740d8b5deb
Miles Zimmerman <miles@houseninja.co>
2022-02-02 21:26:39 -0800

    fix: no auth check on service areas and device create

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,4 +1,6 @@
 class DevicesController < ApplicationController
+  before_action :authenticate_request!, except: [:create, :update]
+
   def index
     devices = DeviceResource.all(params)
     respond_with(devices)
--- app/controllers/service_areas_controller.rb
+++ app/controllers/service_areas_controller.rb
@@ -1,4 +1,6 @@
 class ServiceAreasController < ApplicationController
+  before_action :authenticate_request!, except: [:index, :show]
+
   def index
     service_areas = ServiceAreaResource.all(params)
     respond_with(service_areas)
