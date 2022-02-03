b7518fd7f617b7a684a3baa87583f673275219f8
Miles Zimmerman <miles@houseninja.co>
2022-02-02 22:47:30 -0800

    fix: application typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "app.houseninja.co", "co.houseninja.app", "co.houseninja.applicatio", "co.houseninja.application://"
+    origins "app.houseninja.co", "co.houseninja.app", "co.houseninja.application", "co.houseninja.application://"
 
     resource "*",
       headers: :any,
