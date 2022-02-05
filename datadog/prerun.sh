d882cae9da40408e349c50b5ef967a45e5424ff1
Miles Zimmerman <miles@houseninja.co>
2022-02-05 02:28:35 -0800

    fix: datadog prerun script

---
+++ datadog/prerun.sh
@@ -0,0 +1,4 @@
+# Disable the Datadog Agent based on dyno type
+if [ "$DYNOTYPE" == "run" ] || [ "$DYNOTYPE" == "scheduler" ] || [ "$DYNOTYPE" == "release" ]; then
+  DISABLE_DATADOG_AGENT="true"
+fi
