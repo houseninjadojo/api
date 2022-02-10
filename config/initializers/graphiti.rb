4ad478a171ef0bd230ef4c57782d97af6da629c9
Miles Zimmerman <miles@houseninja.co>
2022-02-10 13:42:54 -0800

    chore: use pagination links in api response payloads

---
+++ config/initializers/graphiti.rb
@@ -0,0 +1,7 @@
+Graphiti.configure do |c|
+  c.respond_to       = [:jsonapi]
+  c.pagination_links = true
+
+  c.debug        = Rails.env.development?
+  c.debug_models = Rails.env.development?
+end
