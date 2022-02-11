65bcf89f5f1e55d526821a10be660a7cecd8c008
Miles Zimmerman <miles@houseninja.co>
2022-02-11 14:33:23 -0800

    fix(db:seed): change supscription plan prices

--- db/seeds/subscription_plans.seeds.rb
+++ db/seeds/subscription_plans.seeds.rb
@@ -1,7 +1,7 @@
 SubscriptionPlan.find_or_create_by({
   slug: 'annual',
   name: 'Annual',
-  price: '539',
+  price: '319',
   interval: 'year',
   perk: '1 month free',
   stripe_price_id: 'annual',
@@ -10,7 +10,7 @@ SubscriptionPlan.find_or_create_by({
 SubscriptionPlan.find_or_create_by({
   slug: 'monthly',
   name: 'Monthly',
-  price: 49,
+  price: 29,
   interval: 'month',
   perk: 'More flexible',
   stripe_price_id: 'monthly',
