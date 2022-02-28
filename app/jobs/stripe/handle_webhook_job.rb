bd18ff858891f08a8abeb188a129ac2cb1455cee
Miles Zimmerman <miles@houseninja.co>
2022-02-28 15:55:43 -0800

    fix(webhooks): wrap stripe webhook job in activerecord transaction

--- app/jobs/stripe/handle_webhook_job.rb
+++ app/jobs/stripe/handle_webhook_job.rb
@@ -8,86 +8,97 @@ class Stripe::HandleWebhookJob < ApplicationJob
 
     case
     when event == "customer.updated"
-      user = User.find_by(stripe_customer_id: stripe_id)
-      return if user.nil?
-      user.update_from_service("stripe", user_attributes)
-      webhook_job.update(processed_at: Time.now)
-    when event == "customer.created"
-      user = User.find_by(stripe_customer_id: stripe_id)
-      if user.present?
+      ActiveRecord::Base.transaction do
+        user = User.find_by(stripe_customer_id: stripe_id)
+        return if user.nil?
+        user.update_from_service("stripe", user_attributes)
         webhook_job.update(processed_at: Time.now)
-        return
       end
-      user = User.new(user_attributes)
-      user.stripe_customer_id = stripe_id
-      # @todo
-      # enable this when ready
-      # user.save!
-      # webhook_job.update(processed_at: Time.now)
-
+    when event == "customer.created"
+      ActiveRecord::Base.transaction do
+        user = User.find_by(stripe_customer_id: stripe_id)
+        if user.present?
+          webhook_job.update(processed_at: Time.now)
+          return
+        end
+        user = User.new(user_attributes)
+        user.stripe_customer_id = stripe_id
+        # @todo
+        # enable this when ready
+        # user.save!
+        # webhook_job.update(processed_at: Time.now)
+      end
     # `invoice.*` except `invoice.deleted`
     when !!event.match(/^invoice\.(?!deleted).*$/)
-      user = User.find_by(stripe_customer_id: object["customer"])
-      subscription = Subscription.find_by(stripe_subscription_id: object["subscription"])
-      payment = Payment.find_by(stripe_id: object["charge"])
-      invoice = Invoice.find_or_create_by(stripe_id: stripe_id)
-      invoice.update(
-        description: object["description"],
-        payment: payment,
-        period_end: Time.at(object["period_end"]),
-        period_start: Time.at(object["period_start"]),
-        status: object["status"],
-        stripe_object: @payload,
-        subscription: subscription,
-        total: object["total"],
-        user: user
-      )
-      if invoice.document.nil? && object["invoice_pdf"].present?
-        asset = URI.open(object["invoice_pdf"])
-        document = Document.create(
-          invoice: invoice,
+      ActiveRecord::Base.transaction do
+        user = User.find_by(stripe_customer_id: object["customer"])
+        subscription = Subscription.find_by(stripe_subscription_id: object["subscription"])
+        payment = Payment.find_by(stripe_id: object["charge"])
+        invoice = Invoice.find_or_create_by(stripe_id: stripe_id)
+        invoice.update(
+          description: object["description"],
+          payment: payment,
+          period_end: Time.at(object["period_end"]),
+          period_start: Time.at(object["period_start"]),
+          status: object["status"],
+          stripe_object: @payload,
+          subscription: subscription,
+          total: object["total"],
           user: user
         )
-        document.asset.attach(io: asset, filename: "invoice.pdf")
+        if invoice.document.nil? && object["invoice_pdf"].present?
+          asset = URI.open(object["invoice_pdf"])
+          document = Document.create(
+            invoice: invoice,
+            user: user
+          )
+          document.asset.attach(io: asset, filename: "invoice.pdf")
+        end
       end
     # `charge.*` except `charge.dispute.*` or `charge.refund.*`
     when !!event.match(/^charge\.[a-z]+(?![.a-z]).*$/)
-      invoice = Invoice.find_by(stripe_id: object["invoice"])
-      payment_method = PaymentMethod.find_by(stripe_token: object["payment_method"])
-      user = User.find_by(stripe_customer_id: object["customer"])
-      payment = Payment.find_or_create_by(stripe_id: stripe_id)
-      payment.update(
-        amount: object["amount"],
-        description: object["description"],
-        invoice: invoice,
-        paid: object["paid"],
-        payment_method: payment_method,
-        refunded: object["refunded"],
-        statement_descriptor: object["statement_descriptor"],
-        status: object["status"],
-        stripe_object: @payload,
-        user: user
-      )
+      ActiveRecord::Base.transaction do
+        invoice = Invoice.find_by(stripe_id: object["invoice"])
+        payment_method = PaymentMethod.find_by(stripe_token: object["payment_method"])
+        user = User.find_by(stripe_customer_id: object["customer"])
+        payment = Payment.find_or_create_by(stripe_id: stripe_id)
+        payment.update(
+          amount: object["amount"],
+          description: object["description"],
+          invoice: invoice,
+          paid: object["paid"],
+          payment_method: payment_method,
+          refunded: object["refunded"],
+          statement_descriptor: object["statement_descriptor"],
+          status: object["status"],
+          stripe_object: @payload,
+          user: user
+        )
+      end
     # `customer.subscription.*`
     when !!event.match(/^customer\.subscription\.[a-z_]+$/)
-      subscription = Subscription.find_by(stripe_subscription_id: stripe_id)
-      if subscription.present?
-        subscription.update(stripe_object: @payload)
-      else
-        nil
+      ActiveRecord::Base.transaction do
+        subscription = Subscription.find_by(stripe_subscription_id: stripe_id)
+        if subscription.present?
+          subscription.update(stripe_object: @payload)
+        else
+          nil
+        end
       end
     # `promotion_code.*`
     when !!event.match(/^promotion_code\.[a-z]+(?![.a-z]).*$/)
-      promo_code = PromoCode.find_or_create_by(stripe_id: stripe_id)
-      promo_code.update(
-        active: object["active"],
-        amount_off: object.dig("coupon", "amount_off"),
-        code: object["code"],
-        coupon_id: object.dig("coupon", "id"),
-        name: object.dig("coupon", "name"),
-        percent_off: object.dig("coupon", "percent_off"),
-        stripe_object: @payload
-      )
+      ActiveRecord::Base.transaction do
+        promo_code = PromoCode.find_or_create_by(stripe_id: stripe_id)
+        promo_code.update(
+          active: object["active"],
+          amount_off: object.dig("coupon", "amount_off"),
+          code: object["code"],
+          coupon_id: object.dig("coupon", "id"),
+          name: object.dig("coupon", "name"),
+          percent_off: object.dig("coupon", "percent_off"),
+          stripe_object: @payload
+        )
+      end
     end
   end
 
