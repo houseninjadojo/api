beb6efe624a816289c533899fca8fb85b348f63f
Miles Zimmerman <miles@houseninja.co>
2022-03-03 12:22:12 -0800

    fix(redis): set SSL verify none in kredis initalizer

---
+++ config/initializers/kredis.rb
@@ -0,0 +1,8 @@
+Kredis::Connections.connections[:shared] = Redis.new(
+  url: ENV['REDIS_URL'],
+  timeout: 2
+  reconnect_attempts: 3
+  ssl_params: {
+    verify_mode: OpenSSL::SSL::VERIFY_NONE,
+  }
+)
