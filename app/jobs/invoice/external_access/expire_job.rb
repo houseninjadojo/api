923452d268231a090085b62054e0de5386d4a592
Miles Zimmerman <miles@houseninja.co>
2022-05-09 23:52:39 -0700

    set hubspot branch payment link to nil when expiring

--- app/jobs/invoice/external_access/expire_job.rb
+++ app/jobs/invoice/external_access/expire_job.rb
@@ -1,8 +1,28 @@
 class Invoice::ExternalAccess::ExpireJob < ApplicationJob
   queue_as :default
 
+  attr_accessor :invoice
+
   def perform(invoice)
-    return if invoice.nil?
+    @invoice = invoice
+    return unless conditions_met?
+
     invoice.expire_external_access!
+    delete_hubspot_branch_payment_link!
+  end
+
+  def conditions_met?
+    [
+      invoice.present?,
+      invoice.work_order.present?,
+      invoice.work_order.hubspot_id.present?,
+    ].all?
+  end
+
+  def delete_hubspot_branch_payment_link!
+    Hubspot::Deal.update!(
+      invoice.work_order.hubpot_id,
+      { "branch_payment_link" => nil }
+    )
   end
 end
