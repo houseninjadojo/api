5f6dc57457c61502dc7d8d5056923bfd8717bfe0
Miles Zimmerman <miles@houseninja.co>
2022-06-06 14:34:56 -0700

    remove dangler

--- app/jobs/stripe/invoices/update_job.rb
+++
@@ -1,7 +0,0 @@
-class Stripe::Invoices::UpdateJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    ActiveSupport::Deprecation.warn("this job will be deleted, use Sync::Invoice::Stripe::Outbound::CreateJob instead")
-  end
-end
