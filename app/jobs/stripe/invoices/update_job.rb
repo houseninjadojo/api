733258f430993ce96044c164ab48b7ddfdc67e4a
Miles Zimmerman <miles@houseninja.co>
2022-06-06 15:10:48 -0700

    fix(sync): invoice create (#132)
    * fix(sync): invoice create
    
    * remove dangler

--- app/jobs/stripe/invoices/update_job.rb
+++
@@ -1,7 +0,0 @@
-class Stripe::Invoices::UpdateJob < ApplicationJob
-  queue_as :default
-
-  def perform(*args)
-    # Do something later
-  end
-end
---
+++ app/jobs/sync/invoice/stripe/outbound/create_job.rb
@@ -0,0 +1,47 @@
+class Sync::Invoice::Stripe::Outbound::CreateJob < ApplicationJob
+  queue_as :default
+
+  attr_accessor :resource
+
+  def perform(resource)
+    @resource = resource
+    return unless policy.can_sync?
+
+    # we have to create invoice line_items on Stripe side first
+    line_item = Stripe::InvoiceItem.create(line_item_params)
+
+    # create the invoice
+    invoice = Stripe::Invoice.create(params)
+
+    # save to db
+    resource.update!(
+      stripe_id: invoice.id,
+      stripe_object: invoice.as_json
+    )
+  end
+
+  def params
+    {
+      auto_advance:           false,
+      collection_method:      "charge_automatically",
+      customer:               resource.user.stripe_id,
+      default_payment_method: resource.user.default_payment_method.stripe_id,
+      description:            resource.description,
+      subscription:           resource.subscription.stripe_id,
+    }
+  end
+
+  def line_item_params
+    {
+      amount:   resource.total,
+      currency: 'usd',
+      customer: resource.user.stripe_id,
+    }
+  end
+
+  def policy
+    Sync::Invoice::Stripe::Outbound::CreatePolicy.new(
+      resource
+    )
+  end
+end
