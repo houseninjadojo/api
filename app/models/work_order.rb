4c53bfb16ec4b3ff0823b68178cbb9f180ad8dd0
Miles Zimmerman <miles@houseninja.co>
2022-06-28 04:11:46 -0700

    trying again

--- app/models/work_order.rb
+++ app/models/work_order.rb
@@ -49,7 +49,7 @@ class WorkOrder < ApplicationRecord
   belongs_to :status,    class_name: "WorkOrderStatus", primary_key: :slug, foreign_key: :status, required: false
   has_one    :invoice,   dependent: :destroy
   has_one    :deep_link, through: :invoice
-  has_one    :user,      through: :property
+  # has_one    :user,      through: :property
 
   # validations
 
