7b045b8efebf984ac135546a297865871e4ddebd
Miles Zimmerman <miles@houseninja.co>
2022-07-05 11:04:19 -0700

    im so fing done with you arrivy

--- app/jobs/sync/webhook/arrivy_job.rb
+++ app/jobs/sync/webhook/arrivy_job.rb
@@ -9,28 +9,10 @@ class Sync::Webhook::ArrivyJob < ApplicationJob
 
     return unless policy.can_sync?
 
-    webhook_event.update!(payload: payload)
-
     Sync::WorkOrder::Arrivy::Inbound::UpdateJob.perform_now(webhook_event)
   end
 
   def policy
     Sync::Webhook::ArrivyPolicy.new(webhook_event)
-  end
-
-  def arrivy_event
-    @arrivy_event ||= Arrivy::Event.new(webhook_event.payload)
-  end
-
-  def payload
-    if arrivy_event.payload.is_a?(String)
-      begin
-        JSON.parse(arrivy_event.payload)
-      rescue
-        arrivy_event.payload
-      end
-    else
-      arrivy_event.payload
-    end
   end
 end
