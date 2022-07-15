ba9a36f64b93cc28b32088750a656890b44670ed
Miles Zimmerman <miles@houseninja.co>
2022-07-15 00:31:06 -0700

    adjust ttl

--- app/jobs/users/generate_onboarding_link_job.rb
+++ app/jobs/users/generate_onboarding_link_job.rb
@@ -1,7 +1,7 @@
 class Users::GenerateOnboardingLinkJob < ApplicationJob
   queue_as :default
 
-  unique :until_expired, runtime_lock_ttl: 1.second, on_conflict: :log
+  unique :until_expired, runtime_lock_ttl: 1.minute, on_conflict: :log
 
   attr_accessor :user
 
