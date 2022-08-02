b14fe01a361816743edfc8b89d459caa09927210
Miles Zimmerman <miles@houseninja.co>
2022-08-02 02:21:08 -0700

    chore(datadog): do not load postgres

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -17,18 +17,18 @@ if [ -n "$REDIS_URL" ]; then
   fi
 fi
 
-# Update the Postgres configuration from above using the Heroku application environment variable
-if [ -n "$DATABASE_URL" ]; then
-  POSTGREGEX='^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)$'
-  if [[ $DATABASE_URL =~ $POSTGREGEX ]]; then
-    sed -i "s/<YOUR HOSTNAME>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR USERNAME>/${BASH_REMATCH[1]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR DBNAME>/${BASH_REMATCH[5]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-    sed -i "s/<YOUR ENV>/${RAILS_ENV}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
-  fi
-fi
+# # Update the Postgres configuration from above using the Heroku application environment variable
+# if [ -n "$DATABASE_URL" ]; then
+#   POSTGREGEX='^postgres://([^:]+):([^@]+)@([^:]+):([^/]+)/(.*)$'
+#   if [[ $DATABASE_URL =~ $POSTGREGEX ]]; then
+#     sed -i "s/<YOUR HOSTNAME>/${BASH_REMATCH[3]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+#     sed -i "s/<YOUR USERNAME>/${BASH_REMATCH[1]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+#     sed -i "s/<YOUR PASSWORD>/${BASH_REMATCH[2]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+#     sed -i "s/<YOUR PORT>/${BASH_REMATCH[4]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+#     sed -i "s/<YOUR DBNAME>/${BASH_REMATCH[5]}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+#     sed -i "s/<YOUR ENV>/${RAILS_ENV}/" "$DD_CONF_DIR/conf.d/postgres.d/conf.yaml"
+#   fi
+# fi
 
 # # set datadog.yaml config
 # cat << 'EOF' >> "$DATADOG_CONF"
