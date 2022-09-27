8556395b245004b0ad64db64f9fdb18fc4614379
Miles Zimmerman <miles@houseninja.co>
2022-09-26 19:05:24 -0700

    remove production log tag override

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,10 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [:request_id]
+  # config.log_tags = {
+  #   request_id: :request_id,
+  #   ip:         :remote_ip,
+  # }
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
