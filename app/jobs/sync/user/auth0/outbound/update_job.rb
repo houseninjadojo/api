e9493e079c3a88e87c87f3edc0ce96c2e51128d1
Miles Zimmerman <miles@houseninja.co>
2022-10-16 17:02:21 -0700

    try auth0 error handler

--- app/jobs/sync/user/auth0/outbound/update_job.rb
+++ app/jobs/sync/user/auth0/outbound/update_job.rb
@@ -10,7 +10,13 @@ class Sync::User::Auth0::Outbound::UpdateJob < Sync::BaseJob
     @user = user
     return unless policy.can_sync?
 
-    AuthZero.client.patch_user(user.auth_id, params)
+    begin
+      AuthZero.client.patch_user(user.auth_id, params)
+    rescue Auth0::NotFound
+      Rails.logger.error("Auth0 - User Not Found for `#{user.auth_id}`", usr: { id: user&.id, email: user&.email })
+    rescue Auth0::HTTPError, Auth0::Exception => e
+      Rails.logger.error("Auth0 - Error Updating User `#{user.auth_id}`", usr: { id: user&.id, email: user&.email }, error: e)
+    end
   end
 
   def params
