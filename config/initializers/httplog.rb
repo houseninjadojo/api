6ddf1eb56359216bbbc05490bd7c8bec9c0a68cc
Miles Zimmerman <miles@houseninja.co>
2022-10-16 19:15:45 -0700

    httplog enable headers & data

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -11,16 +11,16 @@ HttpLog.configure do |config|
   config.severity = Logger::Severity::INFO
 
   # Tweak which parts of the HTTP cycle to log...
-  # config.log_connect   = true
-  # config.log_request   = true
-  # config.log_headers   = false
-  # config.log_data      = true
-  # config.log_status    = true
-  # config.log_response  = true
-  # config.log_benchmark = true
+  config.log_connect   = false
+  config.log_request   = false
+  config.log_headers   = true
+  config.log_data      = true
+  config.log_status    = true
+  config.log_response  = false
+  config.log_benchmark = true
 
   # ...or log all request as a single line by setting this to `true`
-  config.compact_log = true
+  config.compact_log = false
 
   # You can also log in JSON format
   config.json_log = ['production', 'sandbox'].include?(Rails.env)
