93e937b34c5505896f82acad8c62ad59840e0f94
Miles Zimmerman <miles@houseninja.co>
2022-10-16 17:54:09 -0700

    raise e after logging

--- app/jobs/sync/user/auth0/outbound/update_job.rb
+++ app/jobs/sync/user/auth0/outbound/update_job.rb
@@ -3,39 +3,43 @@ class Sync::User::Auth0::Outbound::UpdateJob < Sync::BaseJob
 
   attr_accessor :user, :changeset
 
-  discard_on Auth0::HTTPError do |job, error|
-    Rails.logger.error(
-      "Auth0 - Error Updating User `#{user.auth_id}`",
-      active_job: { id: job&.job_id },
-      usr: { id: user&.id, email: user&.email },
-      error: e
-    )
-  end
-
-  discard_on Auth0::NotFound do |job, error|
-    Rails.logger.error(
-      "Auth0 - User Not Found for `#{user.auth_id}`",
-      active_job: { id: job&.job_id },
-      usr: { id: user&.id, email: user&.email },
-      error: error
-    )
-  end
+  discard_on Auth0::HTTPError, Auth0::Exception, Auth0::NotFound
 
   # @see https://auth0.com/docs/api/management/v2#!/Users/patch_users_by_id
   # @see https://www.rubydoc.info/gems/auth0/Auth0/Api/V2/Users#patch_user-instance_method
   def perform(user, changeset)
     @changeset = changeset
     @user = user
-    return unless policy.can_sync?
 
-     uthZero.client.patch_user(user.auth_id, params)
-    # begin
-    #   AuthZero.client.patch_user(user.auth_id, params)
-    # rescue Auth0::NotFound
-    #   Rails.logger.error("Auth0 - User Not Found for `#{user.auth_id}`", usr: { id: user&.id, email: user&.email })
-    # rescue Auth0::HTTPError, Auth0::Exception => e
-    #   Rails.logger.error("Auth0 - Error Updating User `#{user.auth_id}`", usr: { id: user&.id, email: user&.email }, error: e)
-    # end
+    unless policy.can_sync?
+      Rails.logger.info(
+        "Sync::User::Auth0::Outbound::UpdateJob - Skipping Sync",
+        usr: { id: user.id, email: user.email },
+        active_job: { id: job_id, class: self.class.name },
+        changeset: changeset
+      )
+      return
+    end
+
+    begin
+      AuthZero.client.patch_user(user.auth_id, params)
+    rescue Auth0::NotFound => e
+      Rails.logger.error(
+        "Auth0 - User Not Found for `#{user.auth_id}`",
+        active_job: { id: job&.job_id },
+        usr: { id: user&.id, email: user&.email },
+        error: e
+      )
+      raise e
+    rescue Auth0::HTTPError, Auth0::Exception => e
+      Rails.logger.error(
+        "Auth0 - Error Updating User `#{user.auth_id}`",
+        active_job: { id: job&.job_id, class: self.class.name },
+        usr: { id: user&.id, email: user&.email },
+        error: e
+      )
+      raise e
+    end
   end
 
   def params
--- app/jobs/sync/user/stripe/outbound/update_job.rb
+++ app/jobs/sync/user/stripe/outbound/update_job.rb
@@ -1,32 +1,44 @@
 class Sync::User::Stripe::Outbound::UpdateJob < Sync::BaseJob
   queue_as :default
 
-  retry_on Stripe::APIConnectionError, wait: 5.minutes, attempts: 3 do |job, error|
-    Rails.logger.warn(
-      "Stripe - API Connection Error Updating User `#{user&.stripe_id}`",
-      usr: { id: user&.id, email: user&.email },
-      active_job: { id: job&.job_id },
-      error: error
-    )
-  end
-
-  discard_on Stripe::StripeError do |job, error|
-    Rails.logger.error(
-      "Stripe - Error Updating User `#{user&.stripe_id}`",
-      usr: { id: user&.id, email: user&.email },
-      active_job: { id: job.job_id },
-      error: e
-    )
-  end
-
   attr_accessor :user, :changeset
+
+  retry_on Stripe::APIConnectionError, wait: 5.minutes, attempts: 3
+  discard_on Stripe::StripeError
 
   def perform(user, changeset)
     @changeset = changeset
     @user = user
-    return unless policy.can_sync?
+    
+    unless policy.can_sync?
+      Rails.logger.info(
+        "Sync::User::Stripe::Outbound::UpdateJob - Skipping Sync",
+        usr: { id: user.id, email: user.email },
+        active_job: { id: job_id, class: self.class.name },
+        changeset: changeset
+      )
+      return
+    end
 
-    Stripe::Customer.update(user.stripe_id, params, { idempotency_key: idempotency_key })
+    begin
+      Stripe::Customer.update(user.stripe_id, params, { idempotency_key: idempotency_key })
+    rescue Stripe::APIConnectionError => e
+      Rails.logger.warn(
+        "Stripe - API Connection Error Updating User `#{user&.stripe_id}`",
+        usr: { id: user&.id, email: user&.email },
+        active_job: { id: job_id, class: self.class.name },
+        error: e
+      )
+      raise e
+    rescue Stripe::StripeError => e
+      Rails.logger.error(
+        "Stripe - Error Updating User `#{user&.stripe_id}`",
+        usr: { id: user&.id, email: user&.email },
+        active_job: { id: job_id, class: self.class.name },
+        error: e
+      )
+      raise e
+    end
   end
 
   def params
