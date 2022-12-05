b4727dceb8fb7af9697d2096fb280ab05fa0ec09
Miles Zimmerman <miles@houseninja.co>
2022-12-05 09:15:29 -0800

    multidb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
+  connects_to database: {
+    writing: :primary,
+    reading: :primary_replica
+  }
+
   scope :arrivy, -> { where.not(arrivy_id: nil) }
   scope :hubspot, -> { where.not(hubspot_id: nil) }
   scope :stripe, -> { where.not(stripe_id: nil) }
