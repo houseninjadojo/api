eeb7ff839b2815f325028748b1ee1234392ff56b
Tony Miller <mcfiredrill@gmail.com>
2023-01-30 10:44:17 -0800

    add template id

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -1,6 +1,8 @@
 class ReceiptMailer < ApplicationMailer
   def receipt
+    @template_id = 'd-287df16bd5bb4153b442a9da7bd226b8'
     @document = params[:document]
+    @subject = "Here is your receipt."
     # file name should have date or something?
     attachments['receipt.pdf'] = File.read(@document.asset.path)
     mail(to: @document.user.email, subject: "Your Receipt")
